75d3a3ea39596782720c9cc4268999567ebf0945
Pedro Andrade <andradephc11@gmail.com>
2023-01-17 21:02:23 -0300

    feat: Get all notes

--- backend/src/main/java/com/bluetech/pedro/andrade/MyNotepad/controllers/NoteController.java
+++ backend/src/main/java/com/bluetech/pedro/andrade/MyNotepad/controllers/NoteController.java
@@ -1,14 +1,12 @@
 package com.bluetech.pedro.andrade.MyNotepad.controllers;
 
-import com.bluetech.pedro.andrade.MyNotepad.dtos.ContactDTO;
 import com.bluetech.pedro.andrade.MyNotepad.dtos.NoteDTO;
 import com.bluetech.pedro.andrade.MyNotepad.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -27,5 +25,11 @@ public class NoteController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(noteDTO.getId()).toUri();
         return ResponseEntity.created(uri).body(noteDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<NoteDTO>> getAllNotesPage(Pageable pageable) {
+        Page<NoteDTO> noteDTOPage = noteService.getAllContactsPage(pageable);
+        return ResponseEntity.ok().body(noteDTOPage);
     }
 }
--- backend/src/main/java/com/bluetech/pedro/andrade/MyNotepad/services/NoteService.java
+++ backend/src/main/java/com/bluetech/pedro/andrade/MyNotepad/services/NoteService.java
@@ -4,6 +4,8 @@ import com.bluetech.pedro.andrade.MyNotepad.dtos.NoteDTO;
 import com.bluetech.pedro.andrade.MyNotepad.models.Note;
 import com.bluetech.pedro.andrade.MyNotepad.repositories.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,12 @@ public class NoteService {
         copyDtoToEntity(noteDTO, entity);
         entity = noteRepository.save(entity);
         return new NoteDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<NoteDTO> getAllContactsPage(Pageable pageable) {
+        Page<Note> page = noteRepository.findAll(pageable);
+        return page.map(NoteDTO::new);
     }
 
     private void copyDtoToEntity(NoteDTO noteDTO, Note entity) {
