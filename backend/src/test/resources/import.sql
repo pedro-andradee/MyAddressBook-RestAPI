a20862d913bf4e370aa4ddbd52e6fa24db600a6e
Pedro Andrade <andradephc11@gmail.com>
2023-01-19 12:00:32 -0300

    test: add h2 database configuration and h2 seed

---
+++ backend/src/test/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_contact (email, name, phone_number) values ('paulo@gmail.com', 'Paulo Coelho', '81994528369');
+INSERT INTO tb_contact (email, name, phone_number) values ('joao@gmail.com', 'João Pedro', '81994823567');
+INSERT INTO tb_contact (email, name, phone_number) values ('arthur@gmail.com', 'Arthur da Silva', '81992756324');
+INSERT INTO tb_contact (email, name, phone_number) values ('rodrigo@gmail.com', 'Rodrigo Costa', '81997532311');
+INSERT INTO tb_contact (email, name, phone_number) values ('carmen@gmail.com', 'Carmen Miranda', '81990588367');
+INSERT INTO tb_contact (email, name, phone_number) values ('amanda@gmail.com', 'Amanda Rego', '81993552299');
+INSERT INTO tb_contact (email, name, phone_number) values ('gabriela@gmail.com', 'Gabriela Souza', '81993552977');
+
+INSERT INTO tb_note (date, text) values ('2023-01-17', 'Estudar Vue.js');
+INSERT INTO tb_note (date, text) values ('2023-01-18', 'Fazer CRUD backend');
+INSERT INTO tb_note (date, text) values ('2023-01-18', 'Fazer testes backend');
+INSERT INTO tb_note (date, text) values ('2023-01-19', 'Fazer feira');
+INSERT INTO tb_note (date, text) values ('2023-01-19', 'Pegar filhos na escola');
+INSERT INTO tb_note (date, text) values ('2023-01-19', 'Ligar para o cliente');
