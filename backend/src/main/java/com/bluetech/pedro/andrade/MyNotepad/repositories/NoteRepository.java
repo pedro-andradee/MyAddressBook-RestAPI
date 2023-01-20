5e5229c5c9693f7f91df6a7a5622cedde769c18b
Pedro Andrade <andradephc11@gmail.com>
2023-01-20 10:17:29 -0300

    test: Note Repository Unit Test

--- backend/src/main/java/com/bluetech/pedro/andrade/MyNotepad/repositories/NoteRepository.java
+++ backend/src/main/java/com/bluetech/pedro/andrade/MyNotepad/repositories/NoteRepository.java
@@ -4,7 +4,6 @@ import com.bluetech.pedro.andrade.MyNotepad.models.Note;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
---
+++ backend/src/test/java/com/bluetech/pedro/andrade/MyNotepad/repositories/NoteRepositoryUnitTests.java
@@ -0,0 +1,42 @@
+package com.bluetech.pedro.andrade.MyNotepad.repositories;
+
+import com.bluetech.pedro.andrade.MyNotepad.models.Note;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+@DataJpaTest
+public class NoteRepositoryUnitTests {
+
+    @Autowired
+    private NoteRepository noteRepository;
+    Pageable pageable;
+    LocalDate dateWithDataPersisted;
+    LocalDate dateWithNoDataPersisted;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        pageable = PageRequest.of(0, 10);
+        dateWithDataPersisted = LocalDate.of(2023, 01, 19);
+        dateWithNoDataPersisted = LocalDate.of(2023, 01, 20);
+    }
+
+    @Test
+    public void findByDateShouldReturnNonEmptyPageWhenDataPersistedOnGivenDate() {
+        Page<Note> page = noteRepository.findByDate(dateWithDataPersisted, pageable);
+        Assertions.assertFalse(page.isEmpty());
+    }
+
+    @Test
+    public void findByDateShouldReturnEmptyPageWhenNoDataPersistedOnGivenDate() {
+        Page<Note> page = noteRepository.findByDate(dateWithNoDataPersisted, pageable);
+        Assertions.assertTrue(page.isEmpty());
+    }
+}
